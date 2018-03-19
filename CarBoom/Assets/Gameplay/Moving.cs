c8804cce505f534bef3a305e1c7403c99be495ae
shilsa <30322616+shilsa@users.noreply.github.com>
2018-03-20 01:17:38 +0700

    Moving

---
+++ CarBoom/Assets/Gameplay/Moving.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour {
+
+	static float speed = 1f, v = 5f;
+
+	void Update () {
+		float hor = Input.GetAxis("Horizontal"), ver = Input.GetAxis("Vertical");
+		if (hor > 0) {
+			transform.Translate (Time.deltaTime * v, 0f, 0f);
+
+		} 
+		else if (hor < 0) {
+			transform.Translate (Time.deltaTime*v*(-1), 0f, 0f);
+		}
+		if (ver > 0) {
+			transform.Translate (0f, 0f, Time.deltaTime * v);
+		} else if (ver < 0) {
+			transform.Translate (0f, 0f, Time.deltaTime * v*(-1));
+		}
+	}
+}
