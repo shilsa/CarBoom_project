110dcf71c2d259eadea31523ced6c3a075ca7600
shilsa <30322616+shilsa@users.noreply.github.com>
2018-04-02 00:16:50 +0700

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/shilsa/CarBoom_project"""
    This reverts commit 67afc9bcc83e9c59d25ccc3cf6f0da0da848af00.

--- CarBoom/Assets/Script/Gameplay/ForBoom/Destroy.cs
+++ CarBoom/Assets/Script/Gameplay/ForBoom/Destroy.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour {
 
-	public float lftime = 1f;
+	public float lftime = 2f;
 
 	// Update is called once per frame
 	void Update () {
-		lftime -= Time.deltaTime*3.5f;
+		lftime -= Time.deltaTime;
 		if (lftime <= 0) {
 			Des ();
 		}
 	}
 	void Des(){
 		Destroy (this.gameObject);
-		Spawn.Bombnumcur--;
 	}
 }
