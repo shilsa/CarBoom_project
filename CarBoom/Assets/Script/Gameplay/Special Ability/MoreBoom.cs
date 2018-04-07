e5a701aa72dde6b0b3fd2bfcc75e190b55b231a2
shilsa <dragoonhoome@gmail.com>
2018-04-07 13:56:09 +0700

    fix the null pointer

--- CarBoom/Assets/Script/Gameplay/Special Ability/MoreBoom.cs
+++ CarBoom/Assets/Script/Gameplay/Special Ability/MoreBoom.cs
@@ -2,17 +2,8 @@
 using UnityEngine;
 
 public class MoreBoom : MonoBehaviour {
-	Collision other1;
 	public static int Bombmax = 1;
-	void Start () {
-		OnCollisionEnter (other1);
-	}
-	void Update(){ 
-		
-
-	}
 	void OnCollisionEnter(Collision col)
-
 	{
 		if (col.gameObject.name == "Cube") {
 			Bombmax++;  
