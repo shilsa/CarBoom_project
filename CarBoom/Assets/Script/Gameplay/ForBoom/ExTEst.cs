65c0d449b057d62a1315fbbff06d990a3df44c4b
shilsa <30322616+shilsa@users.noreply.github.com>
2018-04-11 03:44:07 +0700

    Make it destroy shiet

---
+++ CarBoom/Assets/Script/Gameplay/ForBoom/ExTEst.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExTEst : MonoBehaviour {
+	
+		public Transform expandHor1,expandHor2,expandVer1,expandVer2,Bomb;
+		static int Bombcount = 1;
+		float lenghtmax = 5f, counter = 1f;
+		Vector3 HoriEx = new Vector3(0.5f, 0f, 0f);
+		Vector3 VerEx = new Vector3(0f, 0f, 0.5f);
+		Vector3 HoriCenShif = new Vector3(0.25f, 0f, 0f);
+		Vector3 VerCenShif = new Vector3(0f, 0f, 0.25f);
+		void Update () {			
+			counter -= Time.deltaTime;
+			if (counter <= 0)
+			{
+			expand(); 
+			Bombcount = Spawn.Bombnumcur + 1;
+			}
+		if (Spawn.Bombnumcur > 0 && counter <= 0) {
+			Instantiate (expandHor1, Bomb.position, Bomb.rotation);
+			Instantiate (expandHor2, Bomb.position, Bomb.rotation);
+			Instantiate (expandVer1, Bomb.position, Bomb.rotation);
+			Instantiate (expandVer2, Bomb.position, Bomb.rotation);
+			print ("Just testing");
+		}
+		}
+		void expand()
+		{
+			if (expandHor1.localScale.x <= lenghtmax) {	
+				Instantiate (expandHor1, Bomb.position, Bomb.rotation);
+				expandHor1.localScale += HoriEx;
+				expandHor1.localPosition += HoriCenShif;
+			}
+			if (expandHor2.localScale.x <= lenghtmax)
+			{
+				expandHor2.localScale += HoriEx;
+				expandHor2.localPosition -= HoriCenShif;
+			}
+			if (expandVer1.localScale.z <= lenghtmax)
+			{
+				expandVer1.localScale += VerEx;
+				expandVer1.localPosition += VerCenShif;
+			}
+			if (expandVer2.localScale.z <= lenghtmax)
+			{
+				expandVer2.localScale += VerEx ;
+				expandVer2.localPosition -= VerCenShif;
+			}
+		Destroy1 ();
+		}
+		void Destroy1(){
+		if (expandHor1.localScale.x >= lenghtmax) {
+			print (expandHor1.localScale.x);
+			Destroy (expandHor1);
+			Destroy (expandHor2);
+			Destroy (expandVer1);
+			Destroy (expandVer2);
+		}
+	}
+}
+	
--- CarBoom/Assets/Script/Gameplay/ForBoom/Explosion.cs
+++ CarBoom/Assets/Script/Gameplay/ForBoom/Explosion.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour {
     public Transform expandHor1,expandHor2,expandVer1,expandVer2;
-    float lenghtmax = 5.0f, counter = 1f;
+    float lenghtmax = 3f, counter = 1.5f;
+	public static bool Isrightlenght = false;
     Vector3 HoriEx = new Vector3(0.5f, 0f, 0f);
     Vector3 VerEx = new Vector3(0f, 0f, 0.5f);
     Vector3 HoriCenShif = new Vector3(0.25f, 0f, 0f);
@@ -18,24 +19,27 @@ public class Explosion : MonoBehaviour {
 	}
     void expand()
     {
-        if (!expandHor1.localScale.x.Equals(lenghtmax)) {
+		if (expandHor1.localScale.x <= lenghtmax) {
             expandHor1.localScale += HoriEx;
             expandHor1.localPosition += HoriCenShif;
         }
-        if (!expandHor2.localScale.x.Equals(lenghtmax))
+		if (expandHor2.localScale.x <= lenghtmax)
         {
             expandHor2.localScale += HoriEx;
             expandHor2.localPosition -= HoriCenShif;
         }
-        if (!expandVer1.localScale.z.Equals(lenghtmax))
+		if (expandVer1.localScale.z <= lenghtmax)
         {
             expandVer1.localScale += VerEx;
             expandVer1.localPosition += VerCenShif;
         }
-        if (!expandVer2.localScale.z.Equals(lenghtmax))
+		if (expandVer2.localScale.z <= lenghtmax)
         {
             expandVer2.localScale += VerEx ;
             expandVer2.localPosition -= VerCenShif;
         }
+		if(expandHor1.localScale.x >= lenghtmax){
+			Isrightlenght = true;
+		}
     }
 }
