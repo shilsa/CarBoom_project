9e6f7d78db96e2560335a96ce7b36be15fb95a83
shilsa <30322616+shilsa@users.noreply.github.com>
2018-04-29 19:40:14 +0700

    Them hinh

---
+++ CarBoom/Assets/Script/Animation/Rotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		float hor = Input.GetAxis("Horizontal"), ver = Input.GetAxis("Vertical");
+		if (hor > 0) {
+			transform.Rotate (0f, 90f, 0f);
+		} 
+		else if (hor < 0) {
+			transform.Rotate (0f, -90f, 0f);
+		}
+		if (ver > 0) {
+			transform.Rotate (0f, 0f, 0f);
+		} else if (ver < 0) {
+			transform.Rotate (0f, 180f, 0f);
+		}
+	}
+}
