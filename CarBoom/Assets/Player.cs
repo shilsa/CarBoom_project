aff312d058aa718f386d5cad26decb02b7e8a624
An <annguyen16498@gmail.com>
2018-05-05 08:45:53 +0700

    ávasvas

--- CarBoom/Assets/Player.cs
+++ CarBoom/Assets/Player.cs
@@ -4,8 +4,27 @@ namespace AssemblyCSharp
 {
 	public class Player
 	{
+		public string name;
+		public int score;
+
 		public Player ()
 		{
+		}
+
+		public string getName(string name){
+			return name;
+		}
+
+		public int getScore(int score){
+			return score;
+		}
+
+		public void setName(string name){
+			this.name = name;
+		}
+
+		public void setScore(int score){
+			this.score = score;
 		}
 	}
 }
