7542c57a1a0cc553abcdeda49b2b8940278f9427
policest1 <33093547+policest1@users.noreply.github.com>
2018-05-05 19:14:15 +0700

    sum shiet

---
+++ CarBoom/Assets/Script/Gameplay/Special Ability/MorePower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MorePower : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Car")
+        {
+            if (Explosion.lenghtmax < 15f)
+            {
+                Explosion.lenghtmax += 1f;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
