3fc93fc9b4e2f9e68739706f4fbb2ce181bd0fb9
Trần Đào Sĩ Mạnh <Nickybyrne_6198@hotmail.com>
2018-05-05 16:46:16 +0700

    AI

---
+++ CarBoom/Assets/Script/Gameplay/AI/Combine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Script.Gameplay.AI
+{
+    class Combine
+    {
+        private int a;
+        private int b;
+        public Combine(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int getA()
+        {
+            return this.a;
+        }
+        public int getB()
+        {
+            return this.b;
+        }
+    }
+}
