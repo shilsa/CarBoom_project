4de1f40b3cb319c03362b6c392c293a201dc3abd
shilsa <30322616+shilsa@users.noreply.github.com>
2018-05-07 02:19:15 +0700

    Finishing basic function update

--- CarBoom/Assets/Script/Gameplay/ForBoom/Explosion.cs
+++ CarBoom/Assets/Script/Gameplay/ForBoom/Explosion.cs
@@ -5,37 +5,65 @@ using UnityEngine;
 public class Explosion : MonoBehaviour {
     public Transform expandHor1,expandHor2,expandVer1,expandVer2;
 	public float lenghtmax = 3f;
-	public static bool trigger = false, IsrightLenght = false;
+	public static bool H1hit = false, H2hit = false, V1hit = false, V2hit = false;
     Vector3 HoriEx = new Vector3(0.2f, 0f, 0f);
     Vector3 VerEx = new Vector3(0f, 0f, 0.2f);
     Vector3 HoriCenShif = new Vector3(0.1f, 0f, 0f);
     Vector3 VerCenShif = new Vector3(0f, 0f, 0.1f);
     void Update () {
-            expand(); 
-		if (expandHor1.localScale.x >= lenghtmax) {
+		if(!H1hit)
+            expandH1 ();
+		if(!H2hit)
+			expandH2 ();
+		if(!V1hit)
+			expandV1 ();
+		if(!V2hit)
+			expandV2 ();
+		if (expandHor1.localScale.x >= lenghtmax || expandHor2.localScale.x >= lenghtmax || expandVer1.localScale.z >= lenghtmax || expandVer2.localScale.z >= lenghtmax) {
 			Destroy (this.gameObject);
+			rese ();
+		}
+		if (H1hit && H2hit && V1hit && V2hit) {
+			Destroy (this.gameObject);
+			rese ();
 		}
 	}
-    void expand()
+    void expandH1()
     {
 		if (expandHor1.localScale.x <= lenghtmax) {
             expandHor1.localScale += HoriEx;
             expandHor1.localPosition += HoriCenShif;
-        }
-		if (expandHor2.localScale.x <= lenghtmax)
-        {
-            expandHor2.localScale += HoriEx;
-            expandHor2.localPosition -= HoriCenShif;
-        }
-		if (expandVer1.localScale.z <= lenghtmax)
-        {
-            expandVer1.localScale += VerEx;
-            expandVer1.localPosition += VerCenShif;
-        }
-		if (expandVer2.localScale.z <= lenghtmax)
-        {
-            expandVer2.localScale += VerEx ;
-            expandVer2.localPosition -= VerCenShif;
-        }
+        }					
     }
+	void expandH2(){
+		if (expandHor2.localScale.x <= lenghtmax)
+		{
+			expandHor2.localScale += HoriEx;
+			expandHor2.localPosition -= HoriCenShif;
+		}
+	}
+
+	void expandV1(){
+		if (expandVer1.localScale.z <= lenghtmax)
+		{
+			expandVer1.localScale += VerEx;
+			expandVer1.localPosition += VerCenShif;
+		}
+	}
+
+	void expandV2(){
+		if (expandVer2.localScale.z <= lenghtmax)
+		{
+			expandVer2.localScale += VerEx ;
+			expandVer2.localPosition -= VerCenShif;
+		}
+	}
+
+	void rese(){
+		H1hit = false; H2hit = false; V1hit = false; V2hit = false;
+	}
+	public Explosion(){
+
+
+	}
 }
---
+++ CarBoom/Assets/Script/Gameplay/ForBoom/HaultExpand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HaultExpand : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.name == "Explosionn") {
+			Explosion.H1hit = true;
+		}
+		if (col.gameObject.name == "Explosionn (1)") {
+			Explosion.H2hit = true;
+		}
+		if (col.gameObject.name == "Explosionn (2)") {
+			Explosion.V1hit = true;
+		}
+		if (col.gameObject.name == "Explosionn (3)") {
+			Explosion.V2hit = true;
+		}
+	}
+}
