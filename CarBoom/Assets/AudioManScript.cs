471a92d34583ab5de52e3ac1d2546a4f076b05e8
Cá Cunt <kminhngoc@gmail.com>
2018-05-06 16:43:43 +0700

    Fx
    FX

---
+++ CarBoom/Assets/AudioManScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManScript : MonoBehaviour {
+    public static AudioClip dead, kill, powerup, car;
+    static AudioSource auSrc;
+
+	// Use this for initialization
+	void Start () {
+        dead = Resources.Load<AudioClip>("imded");
+        kill = Resources.Load<AudioClip>("enemyded");
+        powerup = Resources.Load<AudioClip>("powup");
+        car = Resources.Load<AudioClip>("drive");
+        auSrc = GetComponent<AudioSource>();
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public static void PlaySound (string clip)
+    {
+        switch (clip)
+        {
+            case ("imded"):
+                auSrc.PlayOneShot(dead);
+                break;
+            case ("enemyded"):
+                auSrc.PlayOneShot(kill);
+                break;
+            case ("powup"):
+                auSrc.PlayOneShot(powerup);
+                break;
+            case ("drive"):
+                auSrc.PlayOneShot(car);
+                break;
+
+
+
+        }
+    }
+}
--- CarBoom/Assets/Script/Gameplay/General/Moving.cs
+++ CarBoom/Assets/Script/Gameplay/General/Moving.cs
@@ -9,14 +9,23 @@ public class Moving : MonoBehaviour {
 	void Update () {
 		if (Input.GetKey(KeyCode.D)) {
 			transform.Translate (Time.deltaTime * v, 0f, 0f);
-		} 
-		else if (Input.GetKey(KeyCode.A)) {
+            AudioManScript.PlaySound("drive");
+
+        }
+        else if (Input.GetKey(KeyCode.A)) {
 			transform.Translate (Time.deltaTime*v*(-1), 0f, 0f);
-		}
-		if (Input.GetKey(KeyCode.W)) {
+            AudioManScript.PlaySound("drive");
+
+        }
+        if (Input.GetKey(KeyCode.W)) {
 			transform.Translate (0f, 0f, Time.deltaTime * v);
-		} else if (Input.GetKey(KeyCode.S)) {
+            AudioManScript.PlaySound("drive");
+
+        }
+        else if (Input.GetKey(KeyCode.S)) {
 			transform.Translate (0f, 0f, Time.deltaTime * v*(-1));
-		}
-	}
+            AudioManScript.PlaySound("drive");
+
+        }
+    }
 }
