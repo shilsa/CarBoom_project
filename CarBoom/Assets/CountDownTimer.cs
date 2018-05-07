71a5214d4a78e8d1478920a6c78d5bb8f15c65e5
An <annguyen16498@gmail.com>
2018-05-07 23:21:52 +0700

    CountDown

---
+++ CarBoom/Assets/CountDownTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CountDownTimer : MonoBehaviour {
+
+	private float secondsLeft = 10f;
+	public static bool timeout = false;
+
+	private void OnGUI(){
+		if( secondsLeft > 0)
+			GUILayout.Label("Timeplay remaining: " + (int)secondsLeft     );
+		else{
+			GUILayout.Label("Time Out!");
+			timeout = true;
+			SceneManager.LoadScene (2);
+		}
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		secondsLeft -= Time.deltaTime;
+	}
+}
--- CarBoom/Assets/Script/Gameplay/General/EndGame.cs
+++ CarBoom/Assets/Script/Gameplay/General/EndGame.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public void endGame(){
+		SceneManager.LoadScene (2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (CountDownTimer.timeout)
+			endGame ();
 	}
 }
