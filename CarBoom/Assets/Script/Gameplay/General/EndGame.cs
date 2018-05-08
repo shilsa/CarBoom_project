1050f98046b7d4a2cb0b70d2242189d4d7255bb4
shilsa <dragoonhoome@gmail.com>
2018-05-08 13:15:11 +0700

    Revert "Merge branch 'AN' into Shilla's"
    This reverts commit d0cfff3ce227798e41ce156125d3995e03696327, reversing
    changes made to 59042681ae497281d878ae085cb350e3c5c5b50f.

--- CarBoom/Assets/Script/Gameplay/General/EndGame.cs
+++ CarBoom/Assets/Script/Gameplay/General/EndGame.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour {
 
-
-
-	public static void endGame(){
-		SceneManager.LoadScene (2);
+    public EndGame() {
+        Debug.Log("GAME OVER");
+    }
+	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 	}
 }
